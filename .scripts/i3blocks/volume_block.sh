cf99b41af5c8fe2ed2e3469f22db35f97c686449
Gácsi László <8laci8@gmail.com>
2023-05-14 23:33:46 +0200

    Added I3 and I3Blocks config with custome scripts Also added conky config what I use for I3 background

---
+++ .scripts/i3blocks/volume_block.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+case $BLOCK_BUTTON in
+  1) amixer -q sset Master,0 toggle ;;
+  3) ~/.scripts/i3/sound_switcher.sh > /dev/null ;;
+  4) amixer -q sset Master,0 2+ unmute ;;
+  5) amixer -q sset Master,0 2- unmute ;;
+esac
+
+if ( amixer sget Master | grep -q off)
+then
+  echo -n -e '\xef\x9a\xa9 '  #   volume-mute
+elif ( amixer get "Speaker" | grep -q off )
+then
+  echo -n -e '\xef\x80\xa5 '  #   Headphones
+else
+  echo -n -e '\xef\x80\xa8 '  #   volume-up
+fi
+amixer sget Master | grep -o [0-9]*%
